4f25e4f25dd2c7accd452f7fdbdb91c467614522
2312598959 <157106358+2312598959@users.noreply.github.com>
2024-02-22 21:23:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,4 @@ api_test = FastAPI()
 
 @api_test.route("/", methods=["GET", "POST", "PUT", "DELETE"])
 async def hello_world(request: Request):
-    query_params = request.query_params
-    # 获取特定查询参数的例子
-    name = query_params.get("name", "World")
-    
-    return {"message": f"Hello, {name}!"}
+    return {"message": f"Hello!"}
