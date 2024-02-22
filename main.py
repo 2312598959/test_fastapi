569005a10cd9d805b24bd2709ff467d28752be8e
2312598959 <157106358+2312598959@users.noreply.github.com>
2024-02-22 20:48:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 
 from fastapi import FastAPI
 
-
+REQUEST_ID_HEADER = "sss"
 api_test = FastAPI()
 
 @api_test.route("/", methods=["GET", "POST", "PUT", "DELETE"])
 async def hello():
+    rid = request.headers.get(REQUEST_ID_HEADER,'N/A')
+    print("FC Invoke Start RequestId: " + rid)
     return {"message": "Hello SharePoint API!"}
