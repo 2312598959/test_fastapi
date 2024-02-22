9a3b731cae60e99c2e7c8cbae5a62bef9995cb5c
2312598959 <157106358+2312598959@users.noreply.github.com>
2024-02-22 19:29:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 """Main application entrypoint."""
-
-import json
-import time
-
-from fastapi import FastAPI, Request
-from loguru import logger
-
-
-api_test = FastAPI()
-
-
-@api_test.get("/")
-async def hello():
-    return {"message": "Hello SharePoint API!"}
+print("Hello World")
