5cc96398478feb3051f3413ac21cde42b130b826
2312598959 <157106358+2312598959@users.noreply.github.com>
2024-02-23 13:46:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 api_test = FastAPI()
 
-@api_test.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+@api_test.get("/invoke")
+def read_root(incoming_request: Request):
+    access_key_id = (incoming_request.headers["x-fc-access-key-id"],)
+    access_key_secret = incoming_request.headers["x-fc-access-key-secret"]
+    security_token = incoming_request.headers["x-fc-security-token"]
+    return {"Hello": "World"}, access_key_id, access_key_secret, security_token
