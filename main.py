ae5c4c654e46fa849166a5b2ae68cf82ca1b6f26
2312598959 <157106358+2312598959@users.noreply.github.com>
2024-02-27 15:28:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,7 @@ from fastapi import FastAPI, Request
 api_test = FastAPI()
 
 
-@api_test.get("/invoke")
-def read_root(incoming_request: Request):
-    access_key_id = (incoming_request.headers["x-fc-access-key-id"],)
-    access_key_secret = incoming_request.headers["x-fc-access-key-secret"]
-    security_token = incoming_request.headers["x-fc-security-token"]
-    # 将凭证信息添加到返回的字典中，但请注意这在生产环境中通常不安全！
-    response_data = {"Hello": "World", 
-                    "access_key_id": access_key_id, 
-                    "access_key_secret": access_key_secret, 
-                    "security_token": security_token}
-    
-    return response_data
+@api_test.get("/")
+def read_root():
+
+    return "hh"
