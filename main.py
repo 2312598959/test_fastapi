e30ff32de50313e4da6796959386d87b7469978a
2312598959 <157106358+2312598959@users.noreply.github.com>
2024-03-28 21:26:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import uvicorn
-import json
 from loguru import logger
 from fastapi import FastAPI, Request
 
 api_test = FastAPI()
 
 
-@api_test.get("/")
 @api_test.post("/invoke")
 async def invoke(incoming_request: Request):
     # Show version
@@ -20,12 +17,23 @@ async def invoke(incoming_request: Request):
 
     print(body)
     # Get incoming request body
-    body = await incoming_request.json()
-    logger.info(f"Request body -> {body}")
+    payload = await incoming_request.json()
+    logger.info(f"Request body -> {payload}")
     logger.info("3.0")
-    # Get incoming payload.json
-    payload = json.loads(body["payload.json"])
-    logger.info(f"payload.json -> {payload}")
+    oss_endpoint = payload["oss_endpoint"]
+    oss_bucket = payload["oss_bucket"]
+    validate_config_excel = payload["validate_config"]
+    oss_error_folder = payload["oss_error_folder"]
+    oss_archive_folder = payload["oss_archive_folder"]
+    print(f"payload -> {oss_endpoint}")
+    print(f"oss_bucket -> {oss_bucket}")
+
+    print(f"validate_config_excel -> {validate_config_excel}")
+
+    print(f"oss_error_folder -> {oss_error_folder}")
+
+    print(f"oss_archive_folder -> {oss_archive_folder}")
+
     # Init temporary credentials
     logger.info("4.0")
     access_key_id = (incoming_request.headers["x-fc-access-key-id"],)
@@ -35,7 +43,3 @@ async def invoke(incoming_request: Request):
     print("access_key_id:", access_key_id)
     print("access_key_secret:", access_key_secret)
     print("security_token:", security_token)
-
-
-if __name__ == "__main__":
-    uvicorn.run(api_test, host="0.0.0.0", port=9000)
