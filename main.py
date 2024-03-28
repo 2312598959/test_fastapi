54bf19167c84f642009171d8fd8d5ea3421f10e6
2312598959 <157106358+2312598959@users.noreply.github.com>
2024-03-28 20:21:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
 api_test = FastAPI()
-@api_test .get("/")
-@api_test .get("/invoke")
+@api_test.get("/")
+@api_test.get("/invoke")
 @api_test.post("/invoke")
 async def invoke():
     print('hello')
